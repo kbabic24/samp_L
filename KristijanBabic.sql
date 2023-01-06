4874340b41aa098e8119bfaf79ff0e40531a4d1d
kbabic24 <120247405+kbabic24@users.noreply.github.com>
2023-01-06 13:04:26 +0100

    Update KristijanBabic.sql

--- KristijanBabic.sql
+++ KristijanBabic.sql
@@ -1,23 +1,20 @@
--- obriši tablicu gradovi ako postoji
+
 DROP TABLE gradovi;
 
--- napravi tablicu gradovi
 CREATE TABLE gradovi (
     id INT PRIMARY KEY,
     naziv_grada VARCHAR(50) NOT NULL,
     postbr CHAR(5) UNIQUE
 );
 
--- dodaj 2 retka u tablicu gradovi
 INSERT INTO gradovi (id, naziv_grada, postbr)
     VALUES (1, 'Zagreb', '10000');
 INSERT INTO gradovi (id, naziv_grada, postbr)
     VALUES (2, 'Osijek', '31000');
 
--- obriši tablicu zaposlenici ako postji
 DROP TABLE zaposlenici;
 
--- napravi tablicu zaposlenici
+
 CREATE TABLE zaposlenici (
     id INT PRIMARY KEY,
     ime VARCHAR(50) NOT NULL,
@@ -30,7 +27,6 @@ CREATE TABLE zaposlenici (
     visina FLOAT
 );
 
--- dodaj 2 retka u tablicu zaposlenici
 INSERT INTO zaposlenici (id, ime, prezime, oib, datum_rodenja)
     VALUES (1, 'Pero', 'Peric', '12321232123', DATE '1991-01-01');
 
